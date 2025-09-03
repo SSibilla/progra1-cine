5a3f53c3f7f252e17a5c265d044f54f5afcd8675
SSibilla <ssibilla@uade.edu.ar>
2025-09-03 15:10:39 -0300

    Revert "Merge branch 'main' of https://github.com/SSibilla/progra1-cine"
    This reverts commit 19b0fdf0758d494b753801b70b172106a71bdd62, reversing
    changes made to 50e326f26648f53bd071176d3914c010138a482b.

--- CINE PLUS.py
+++ CINE PLUS.py
@@ -1,7 +1,7 @@
 
 import random
 letras = ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
-numeros_filas = [str(i) for i in range(1, 7)]
+numeros_filas=["1","2","3","4","5","6"]
 
 #creo SALA de cine e inicializo la SALA en libre
 def crear_sala():
