572b1f9eed9b7b385843f575d3322bd0a18bd76e
carinamilano <carinamilano@gmail.com>
2025-09-07 15:22:41 -0300

    una pequena correcion

--- CINE_PLUS.py
+++ CINE_PLUS.py
@@ -1,4 +1,3 @@
-
 import random
 
 #creo SALA de cine e inicializo la SALA en libre
@@ -10,14 +9,12 @@ def crear_sala():
             matriz[fila].append("L")
     return matriz
 
-
 #imprimo en pantalla la SALA, pongo las letras como columnas para visualizar + facil
 def mostrar_sala(matriz):
     print ("CINEPLUS SALA de cine")
     print ("  A B C D E F G H I")
     for i,fila in enumerate(matriz):
         print(i+1," ".join(fila)) #join une todos los elementos de la fila y los separa con un espacio
-
 
 #simular ocupacion de la SALA
 def precarga_aleatoria(matriz):
@@ -38,14 +35,11 @@ def reservar(matriz,letras,numeros_filas):
     butaca=input("Ingrese butaca a reservar, filas 1 a 6, posicion A a I , 0 para terminar: ")
     while butaca!="0":
         butaca = validar_butaca(butaca,letras,numeros_filas)
-
         if butaca != "0":
             numero = butaca[:-1]
             letra = butaca[-1]
             f = int(numero) - 1
             c = letras.index(letra)
-
-        
             if matriz[f][c]=="L":
                 if numero in ("3", "4"):
                     valor = 2000
@@ -53,7 +47,6 @@ def reservar(matriz,letras,numeros_filas):
                     valor = 1500
                 print(f"Se ha reservado butaca: {numero}{letra} - ${valor}")
                 matriz[f][c]="R"
-
             else:
                 print("Butaca no disponible!")
             butaca = input("Ingrese butaca a reservar, filas 1 a 6, posicion A a I , 0 para terminar: ")
@@ -63,27 +56,20 @@ def cancelar_reserva(matriz,letras,numeros_filas):
     mostrar_sala(matriz)
     mostrar_butacas_reservadas(matriz,numeros_filas,letras)
     butaca=input("Ingrese butaca que desea cancelar la reserva (Ej 3C), filas 1 a 6, posicion A a I , 0 para terminar: ")
-
     while butaca!="0":
         butaca = validar_butaca(butaca,letras,numeros_filas)
-
-
         if butaca != "0":
             numero = butaca[:-1]
             letra = butaca[-1]
             f = int(numero) - 1
             c = letras.index(letra)
-            
             if matriz[f][c]=="R":
                 print("Se ha cancelado su reserva con exito")
                 matriz[f][c]="L"
-
             else:
                 print("La butaca elegida no esta reservada!")
-            
             mostrar_butacas_reservadas(matriz,numeros_filas,letras)
             butaca = input("Ingrese butaca que desea cancelar la reserva (Ej 3C), filas 1 a 6, posicion A a I , 0 para terminar: ")
-
 
 def validar_opcion_menu(opcion):
     posibilidades=["0","1","2","3","4","5","6","7"]
@@ -95,7 +81,6 @@ def validar_butaca(butaca,letras,numeros_filas):
     butaca=butaca.upper()
     letra=butaca[-1]
     numero=butaca[:-1]
-
     while (letra not in letras or numero not in numeros_filas) and butaca != "0":
         print("⚠️ Entrada inválida. Use número+letra (ej: 3C).")
         butaca = input("Ingrese butaca (ej. 3C) o 0 para salir: ").upper()
@@ -107,13 +92,11 @@ def validar_butaca(butaca,letras,numeros_filas):
         else:
             numero = ""
             letra = ""
-
     return butaca
 
 def mostrar_butacas_reservadas(matriz,numeros_filas,letras):
     butacas_reservadas=[]
     valores = []
-
     for i in range (len(matriz)):
         for j in range(len(matriz[i])):
             if matriz[i][j] == "R":
@@ -122,12 +105,10 @@ def mostrar_butacas_reservadas(matriz,numeros_filas,letras):
                     valores.append (2000)
                 else:
                     valores.append (1500)        
-
     if len(butacas_reservadas) >0:
         print(f"BUTACAS RESERVADAS: {len(butacas_reservadas)}")
         for k in range (len(butacas_reservadas)):
             print(f"{butacas_reservadas[k]} ${valores[k]}")
-    
     else:
         print("NO HAY BUTACAS RESERVADAS.")
 
@@ -144,7 +125,6 @@ def confirmar_venta(matriz, recaudacion,butacas_vendidas,numeros_filas, letras):
         if matriz[f][c] == "R":
             print("Su compra ha sido CONFIRMADA")
             butacas_vendidas.append (f"{numeros_filas[f]}{letras[c]}")
-
             matriz[f][c] = "O"
             if numero in (3, 4):
                 recaudacion += 2000
@@ -152,27 +132,21 @@ def confirmar_venta(matriz, recaudacion,butacas_vendidas,numeros_filas, letras):
                 recaudacion += 1500
             mostrar_butacas_reservadas(matriz,numeros_filas,letras)
             butaca=input("Ingrese reserva que desea abonar, Ej: 1C (filas 1 a 6, posicion A a I) 0 para terminar: ")
-
         else:
             print("La butaca seleccionada no se encuentra reservada!")
             butaca = input("Ingrese reserva que desea abonar, Ej: 1C (filas 1 a 6, posicion A a I) 0 para terminar: ")
     return recaudacion
 
 def buscar_butacas_juntas(matriz,letras):
-    
     filas = len(matriz)
     cols = len(matriz[0])
-
     s = input("¿Cuántas butacas seguidas desea? (1-" + str(cols) + "): ")
     while (not s.isdigit()) or (int(s) < 1) or (int(s) > cols):
         print("⚠️ Opción inválida: ingrese un número entre 1 y " + str(cols) + ".")
         s = input("¿Cuántas butacas seguidas desea? (1-" + str(cols) + "): ")
-
     n = int(s)
-
     encontrado = False
     seguir_busqueda = True
-
     f = 0
     while f < filas and seguir_busqueda:
         c = 0
@@ -185,24 +159,19 @@ def buscar_butacas_juntas(matriz,letras):
                     c += 1
                 if contador >= n:
                     encontrado = True
-
-
                     inicio_butaca = str(f+1) + letras[inicio]
                     fin_butaca = str(f+1) + letras[inicio+n-1]
                     sugerencia = inicio_butaca + "-" + fin_butaca
                     print("Sugerencia:", sugerencia)
-
                     opcion = input("¿Desea reservar la secuencia de butacas? (s/n): ").lower()
                     while opcion != "s" and opcion != "n":
                         print ("⚠️Opción inválida. Ingrese s(SI) o n(NO).")
                         opcion = input("¿Desea reservar la secuencia de butacas? (s/n): ").lower()
-
                     if opcion == "s":
                         for i in range(inicio, inicio+n):
                             matriz[f][i] = "R"
                         print("Bloque reservado correctamente:", sugerencia)
                         encontrado = True
-
                         otra = input(f"¿Quiere reservar otro bloque de {n} butacas? (s/n): ").lower()
                         while otra != "s" and otra != "n":
                             print ("⚠️Opción inválida. Ingrese s(SI) o n(NO).")
@@ -211,17 +180,13 @@ def buscar_butacas_juntas(matriz,letras):
                             seguir_busqueda = False
                         else:
                             encontrado = False
-                            
                     else:
                         seguir_busqueda = False
             else:
                 c += 1
         f += 1
-
     if not encontrado:
         print(f"No se encontraron más secuencias de {n} butacas disponibles. Volviendo al menú...")
-
-
 
 def mostrar_recaudacion (recaudacion,butacas_vendidas):
     if len(butacas_vendidas)>0:
@@ -229,7 +194,6 @@ def mostrar_recaudacion (recaudacion,butacas_vendidas):
         for i in range (len(butacas_vendidas)):
             print (f"{butacas_vendidas[i]}")
         print ("La recaudacion diaria de la sala es: $", recaudacion)
-    
     else:
         print ("NO HUBO BUTACAS VENDIDAS")
         print ("La recaudacion diaria de la sala es: $", recaudacion)
@@ -238,31 +202,25 @@ def mostrar_estadisticas (matriz, numeros_filas):
     libres     = sum(1 for fila in matriz for x in fila if x == "L")
     ocupadas   = sum(1 for fila in matriz for x in fila if x == "O")
     reservadas = sum(1 for fila in matriz for x in fila if x == "R")
-
     total_butacas = len(matriz) * len(matriz[0])
     ocupacion_total = ocupadas + reservadas
     porcentaje = round((ocupacion_total * 100) / total_butacas) if total_butacas > 0 else 0
-
     # Ocupación por fila (O + R por fila)
     ocup_por_fila = []
     for i, fila in enumerate(matriz):
         cant = sum(1 for x in fila if x in ("O", "R"))
         ocup_por_fila.append((i, cant))
-
     # Fila con más y menos ocupación
     fila_mas   = max(ocup_por_fila, key=lambda t: t[1])
     fila_menos = min(ocup_por_fila, key=lambda t: t[1])
-
     # Ranking de filas por ocupación
     ranking = ocup_por_fila[:]  # copio
     ranking.sort(key=lambda t: (-t[1], t[0]))  # primero más ocupadas, empate por índice
-
     ranking_mostrable = []
     for par in ranking:
         indice_fila = par[0]                 
         numero_de_fila = int(numeros_filas[indice_fila])  
         ranking_mostrable.append(numero_de_fila)
-
     print("\n=== ESTADÍSTICAS ===")
     print(f"Butacas libres: {libres}")
     print(f"Butacas ocupadas: {ocupadas}")
@@ -284,10 +242,8 @@ def menu(letras,numeros_filas,SALA,recaudacion,butacas_vendidas):
         print("6) Estadísticas")
         print("7) Recaudación")
         print("0) Salir")
-
         opcion = input("Elegí una opción: ")
         opcion = validar_opcion_menu(opcion)
-
         if opcion == "1":
             mostrar_sala(SALA)
         elif opcion == "2":
@@ -300,13 +256,10 @@ def menu(letras,numeros_filas,SALA,recaudacion,butacas_vendidas):
             recaudacion=confirmar_venta(SALA, recaudacion,butacas_vendidas,numeros_filas, letras)
         elif opcion == "5":
             buscar_butacas_juntas(SALA, letras)
-                        
         elif opcion == "6":
             mostrar_estadisticas (SALA, numeros_filas)
-
         elif opcion == "7":
             mostrar_recaudacion (recaudacion, butacas_vendidas)
-
         elif opcion == "0":
             print("¡Gracias por usar CINEPLUS! 👋")
         else:
@@ -317,11 +270,9 @@ def main ():
     letras = ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
     numeros_filas=["1","2","3","4","5","6"]
     butacas_vendidas = []
-
     SALA=crear_sala()
     precarga_aleatoria(SALA)
     recaudacion=0
-
     menu (letras,numeros_filas,SALA,recaudacion,butacas_vendidas)
 
 main()
